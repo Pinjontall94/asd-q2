bb4efd191d7e48c1ed4f2eab5bbbb85e137970ae
Sam Johnson <sambjohnson94@protonmail.com>
2021-10-19 20:53:00 -0600

    fixed manifest gen grabbing duplicate vals

--- scripts/manifest_gen.py
+++ scripts/manifest_gen.py
@@ -33,9 +33,21 @@ fastqs = tuple(os.listdir(fastq_dir))
 
 # Create tuple of sample id's from fastqs, minus their suffices and file extensions
 #   (e.g. 'ERR1353528')
-sample_id = tuple(map(lambda x: os.path.splitext(x)[0], fastqs))
 
-fastqs_abs_path = tuple(set(map(lambda x: "$PWD/" + fastq_dir + x, fastqs)))
+# Extracts substrings from fastqs as re.match objects
+substrings = [re.search(".*(?=_R?[12]\.fastq)", i) for i in fastqs]
+
+# Get a list from the list of re.match objects (so it can be used in the dataframe later)
+sample_strings = list(map(lambda x: x.group(), substrings))
+
+# Remove redundant entries from the reverse reads by taking only unique values in sample_strings
+sample_id = list(dict.fromkeys(sample_strings))
+
+# NOTE: Doesn't work, splitext leaves the read direction attached.
+# Use re.search instead to extract the sample id substring.
+# sample_id = tuple(set(map(lambda x: os.path.splitext(x)[0], fastqs)))
+
+fastqs_abs_path = tuple(map(lambda x: "$PWD/" + fastq_dir + x, fastqs))
 
 forward = tuple(filter(lambda x: re.search(r"_R?1\.fastq", x), fastqs_abs_path))
 
@@ -50,6 +62,10 @@ if len(forward) > 0 and len(reverse) > 0:
     }
 
     # Import the dictionary into a dataframe
+    print("sample_id =", sample_id)
+    print("fastqs_abs_path = ", fastqs_abs_path)
+    print("forward =", forward)
+    print("reverse =", reverse)
     df = pd.DataFrame(d)
 
     # Export the dataframe to a file labeled manifest.tsv
